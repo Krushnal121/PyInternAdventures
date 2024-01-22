2aad8e22f02bbfe488ed7bcb3d36029b0e9b83ec
Krushnal Jagannath Patil <krushnalpatil1111@gmail.com>
2024-01-22 16:00:38 +0530

    Commit

--- Assignment III/noduplicate.py
+++ Assignment III/noduplicate.py
@@ -1,1 +1,8 @@
+#Approach 01
 print(f"The list excluding duplicate values is as follows: {set([x for x in input('Approach 01: Enter the strings seperated by a space: ').split()])}")
+
+#Approach 02
+outputlist=[]
+for x in [x for x in input("Approach 02: Enter the strings: ").split()]:
+    if x not in outputlist:outputlist.append(x)
+print(f"The list excluding duplicate values is as follows:{outputlist}")
