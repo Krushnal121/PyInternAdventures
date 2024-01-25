00f04d22f5b6d617aadbd07994e5a321aa5ac66f
Krushnal Jagannath Patil <krushnalpatil1111@gmail.com>
2024-01-25 22:18:07 +0530

    commit

--- HackerRank Practice/HackerRank#22_Text_Wrap.py
+++ HackerRank Practice/HackerRank#22_Text_Wrap.py
@@ -1,18 +1,8 @@
 import textwrap
-lst1=[]
-lst2=[]
+
 def wrap(string, max_width):
-    length = len(string)
-    lst1=[string[i] for i in range(length)]
-    for i in range (0,length,max_width):
-        if (i+max_width)!=length:
-            lst2.append(string[i:i+4])
-            lst2.append("\n")
-        else:
-            lst2.append(string[i:])
-    if lst2[-1]=="\n":lst2.pop()
-    lststr="".join(lst2)
-    return lststr
+    outstr="\n".join(textwrap.TextWrapper(width=max_width).wrap(text=string))
+    return outstr
 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
