7cd858942bfa9cf9e7b93a31f2a08f835d9d4ccd
Krushnal Jagannath Patil <krushnalpatil1111@gmail.com>
2024-01-31 21:34:59 +0530

    commit

---
+++ Assignment VI/SecondSmallest.py
@@ -0,0 +1,2 @@
+#Program to print sencond smallest number in list
+print(f"The second smallest number in the given list is: {(sorted([int(input()) for x in range(int(input('Enter the numbers of Elements you want to enter and enter the respective numbers on next line: ')))]))[1]}")
