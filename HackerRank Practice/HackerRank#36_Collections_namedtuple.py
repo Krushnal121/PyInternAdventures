de08f6c5394868ffa8a320216646efb001dec6aa
Krushnal Jagannath Patil <krushnalpatil1111@gmail.com>
2024-02-04 22:49:30 +0530

    commit

---
+++ HackerRank Practice/HackerRank#36_Collections_namedtuple.py
@@ -0,0 +1,24 @@
+'''
+calculate the average marks of the students.
+sample data:
+
+ID         MARKS      NAME       CLASS
+1          97         Raymond    7
+2          50         Steven     4
+3          91         Adrian     9
+4          72         Stewart    5
+5          80         Peter      6
+
+Problem link: https://www.hackerrank.com/challenges/py-collections-namedtuple/problem
+'''
+
+from collections import namedtuple
+count = int(input())
+columns = input().split()
+marks_sum = 0
+for _ in range(count):
+    students = namedtuple('student', columns)
+    MARKS, CLASS, NAME, ID = input().split()
+    student = students(MARKS, CLASS, NAME, ID)
+    marks_sum += int(student.MARKS)
+print((marks_sum / count))
