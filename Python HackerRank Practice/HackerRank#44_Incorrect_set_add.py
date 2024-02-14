5c67921f059f5c0b8b64c0b20b52f8e2b437c6aa
[Krushnal121] <krushnalpatil1111@gmail.com>
2024-02-14 19:02:00 +0530

    commit

---
+++ Python HackerRank Practice/HackerRank#44_Incorrect_set_add.py
@@ -0,0 +1,11 @@
+'''
+Apply your knowledge of the .add() operation to help your friend Rupal.
+
+Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. She asked for your help. You pick the stamps one by one from a stack of N country stamps.
+
+Find the total number of distinct country stamps.
+
+Problem link: https://www.hackerrank.com/challenges/py-set-add/problem
+'''
+
+print(len(set([input() for i in range (int(input()))])))
