066d2e1f5919d62e1a065b491cb9207740657951
[Krushnal121] <krushnalpatil1111@gmail.com>
2024-02-27 13:42:24 +0530

    commit

---
+++ Python HackerRank Practice/HackerRank#56_Integers_Come_In_All_Sizes.py
@@ -0,0 +1,7 @@
+'''
+Read four numbers, a, b, c, and d, and print the result of a^b+c^d.
+
+Problem Link: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+'''
+
+print((int(input())**int(input()))+(int(input())**int(input())))
