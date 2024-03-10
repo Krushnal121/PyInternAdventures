671286fc30b47e696164f310e033e39063778998
[Krushnal121] <krushnalpatil1111@gmail.com>
2024-03-10 10:11:54 +0530

    commit

---
+++ SQL HackerRank Practice/HackerRank#39_Draw_The_Triangle_2.sql
@@ -0,0 +1,6 @@
+/*Problem Link:https://www.hackerrank.com/challenges/draw-the-triangle-2/problem*/
+
+SET @TEMP:=0;
+SELECT REPEAT('* ', @TEMP:= @TEMP + 1)
+FROM INFORMATION_SCHEMA.TABLES
+WHERE @TEMP < 20;
